1331472c4902758b4fd48dc0d4f6123fccff088c
류재현 <you@example.com>
2019-01-02 18:29:59 +0900

    업로드

---
+++ java-basic/src/main/java/ch01/Test02.java
@@ -0,0 +1,7 @@
+package ch01;
+
+public class Test002 {
+public static void main(String[] args) {
+   System.out.println("Hollo") ;
+     }
+}
---
+++ java-basic/src/main/java/ch01/Test03.java
@@ -0,0 +1,16 @@
+package ch01;
+
+public class Test003 {
+/**ㅇㅇㅇㅇ
+ */
+@Override
+public String toString()) {
+  return "ok" ; 
+   
+}
+   public static void main(String[] args) {
+   System.out.println("Hollo") ;
+     }
+}
+
+
